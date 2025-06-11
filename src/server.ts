17c07d605ced900b9ee70fac6dc35f3cddf4a575
Sheikh Jabed <jabedbd2295@gmail.com>
2025-06-11 22:57:13 +0600

    [UPDATED]: Activities and functionalities related to app and server setup done

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-// Server Setup ----
+import app from "./app";
+import { client } from "./config/mongodb";
+
+let server;
+const port = 5000;
+
+const tailwind = async () => {
+  await client.connect();
+  console.log("Connected to MongoDB");
+
+  server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+tailwind();
